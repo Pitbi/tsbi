43929810720e9ba708965d864fea5428254dc909
Pitbi <Pitbi@Pitbis-MacBook-Pro.local>
2013-07-24 17:17:51 +0200

    Add mail system need verification with TSBI

---
+++ config/config.production.js
@@ -0,0 +1,9 @@
+module.exports= {
+  smtp: {
+    user: "b272d948b50d5be07803c1b7479c0358",
+    password: "4858cbda47cd534335ee5d243aed8336",
+    host: "in.mailjet.com",
+    sender: "Racely <noreply@racely.net>"
+  }
+};
+
--- controllers/contact_controller.js
+++ controllers/contact_controller.js
@@ -1,3 +1,7 @@
+var ENVIRONMENT 	= process.env.NODE_ENV || "dev";
+var config 				= require('../config/config' + '.' + ENVIRONMENT);
+var email    = require("emailjs/email");
+
 var ContactController = function(req, res, next) {
   this.res = res;
   this.req = req;
@@ -10,4 +14,33 @@ ContactController.prototype.GET = function () {
   self.res.render("contact/show");
 };
 
+ContactController.prototype.POST = function () {
+  var self= this;
+  var attributes = self.req.body;
+  console.log(attributes);
+  sendOfferByMail(attributes, function (err) {
+  	if (err) throw err;
+
+  	
+  	self.res.redirect("back");
+  });
+};
+
+
 module.exports = ContactController;
+
+
+var sendOfferByMail = function(attributes, callback) {
+  var server  = email.server.connect({
+    user: config.smtp.user,
+    password: config.smtp.password,
+    host: config.smtp.host,
+    ssl: true
+  });
+  server.send({
+    text:    attributes.message,
+    from:    config.smtp.sender,
+    to:      "pierre.biezemans@gmail.com",
+    subject: "TSBI.be: " + attributes.subject
+  }, callback);
+};
